2f3e9efc33f7b681e74886ffccd9f2fbb0add29c
enthbets-commits <enth.bets@gmail.com>
2026-03-29 13:31:54 -0400

    Added API key and fetch

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "mwSJtuDbG0UKBadARkMhXt2i7BAPmRts";
+
+const button = document.querySelector("#fetch-gif-btn");
+const gifContainer = document.querySelector("#gif-container");
+const searchInput = document.querySelector("#search-input");
+
+button.addEventListener("click", async () => {
+  const searchTerm = searchInput.value || "funny";
+
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=8`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  gifContainer.innerHTML = "";
+
+  data.data.forEach(gif => {
+    gifContainer.innerHTML += `
+      <img src="${gif.images.original.url}">
+    `;
+  });
+});
